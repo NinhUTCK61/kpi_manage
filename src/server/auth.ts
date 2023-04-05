4314b95540ec09eb4422d61103f537497722a29e
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-04-05 11:13:46 +0700

    feat(ninh_solashi): dont change

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -114,6 +114,7 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  secret: env.NEXTAUTH_SECRET,
   session: { strategy: 'jwt' },
 }
 
