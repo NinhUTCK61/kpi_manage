125ddfb78d17b338ce9c46ba03607c05d057f7c0
tranthinh201 <console.namee@gmail.com>
2023-04-07 11:51:31 +0700

    feat(KM-7): init forgot password

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,11 +1,31 @@
-import { z } from 'zod'
-
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
+import nodemailer from 'nodemailer'
+import { z } from 'zod'
 
 export const authRouter = createTRPCRouter({
   login: publicProcedure
     .input(z.object({ email: z.string().email(), password: z.string() }))
     .query(async ({ input, ctx: { prisma } }) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: input.email,
+        },
+      })
+      const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_SERVER_HOST,
+        pool: true,
+        port: process.env.EMAIL_SERVER_PORT ? parseInt(process.env.EMAIL_SERVER_PORT, 10) : 2525,
+        secure: false, // upgrade later with STARTTLS
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      })
+      return user
+    }),
+  forgotPassword: publicProcedure
+    .input(z.object({ email: z.string().email(), password: z.string() }))
+    .mutation(async ({ input, ctx: { prisma } }) => {
       const user = await prisma.user.findUnique({
         where: {
           email: input.email,
