2f6d08ce76b3cd55fc85379a148e9e651af0e6b2
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-04-09 21:29:22 +0700

    feat(KM-6): update code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession()
-
   return (
     <Layout title="KPI Master">
       <p>{sessionData && <span>Logged in as {sessionData.user?.name}</span>}</p>
