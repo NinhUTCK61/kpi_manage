57d62d199a1bd6aa6d0f2536b6373f812aa278b3
NinhFe <ninhtq.fe@consultindochina.com>
2023-04-10 15:51:16 +0700

    feat(KM-16): update validation schema signup

--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -1,6 +1,12 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string(),
-});
+})
+
+export const SignUpSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string().max(255),
+})
