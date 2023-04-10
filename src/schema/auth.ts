c5a021742ee778e59fae9eaa154c157ddadc2420
NinhFe <ninhtq.fe@consultindochina.com>
2023-04-10 15:51:16 +0700

    feat(KM-16): update validation schema signup

--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -6,3 +6,22 @@ export const LoginSchema = z.object({
 })
 
 export type SignInType = z.infer<typeof LoginSchema>
+
+const uppercaseRegex = /[A-Z]/
+const lowercaseRegex = /[a-z]/
+const numberRegex = /[0-9]/
+const specialCharRegex = /[!@#$%^&*()_+]/
+
+export const SignUpSchema = z.object({
+  email: z.string().email({
+    message: 'Invalid email address!',
+  }),
+  password: z
+    .string()
+    .min(8, 'Minimum length of 8 characters')
+    .regex(specialCharRegex, 'Must include at least one special character (!,@,#,$,%,^,&,*,(,))')
+    .regex(uppercaseRegex, 'Must include at least one uppercase letter')
+    .regex(lowercaseRegex, 'Must include at least one lowercase letter')
+    .regex(numberRegex, 'Must include at least one number'),
+  name: z.string().max(255),
+})
