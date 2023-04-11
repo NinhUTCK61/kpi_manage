7dcace83f938aeb00ebf52fa1ff55746bb9b7f76
tranthinh201 <console.namee@gmail.com>
2023-04-11 10:34:45 +0700

    feat(KM-31): fix error send mail

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,4 +1,3 @@
-import { TRPCError } from '@trpc/server'
 import nodemailer, { SendMailOptions, Transporter } from 'nodemailer'
 
 interface MailOptions {
@@ -32,36 +31,22 @@ class MailUtils {
   }
 
   async sendMail(options: MailOptions): Promise<void> {
-    try {
-      const mailOptions: SendMailOptions = {
-        from: process.env.EMAIL_FROM,
-        to: options.to,
-        subject: options.subject,
-        html: options.html,
-      }
-      await this.transporter.sendMail(mailOptions)
-    } catch (error) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Send mail failed!',
-      })
+    const mailOptions: SendMailOptions = {
+      from: process.env.EMAIL_FROM,
+      to: options.to,
+      subject: options.subject,
+      html: options.html,
     }
+    await this.transporter.sendMail(mailOptions)
   }
 
   async sendPasswordResetMail(email: string, token: string): Promise<void> {
-    try {
-      const resetLink = `${process.env.NEXTAUTH_URL}/change-password/${token}`
-      const subject = 'Password Reset'
-      const html = `<h1><a href="${resetLink}">${token}</a></h1>`
+    const resetLink = `${process.env.NEXTAUTH_URL}/change-password/${token}`
+    const subject = 'Password Reset'
+    const html = `<h1><a href="${resetLink}">${token}</a></h1>`
 
-      const mailer = await this.sendMail({ to: email, subject, html })
-      return mailer
-    } catch (error) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Send mail failed!',
-      })
-    }
+    const mailer = await this.sendMail({ to: email, subject, html })
+    return mailer
   }
 }
 
