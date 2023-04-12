3f58f37748112e41d215a3910fc923f09da0f1c6
tranthinh201 <console.namee@gmail.com>
2023-04-12 15:57:06 +0700

    feat(KM-32): update UI template send mail

--- src/screens/Mail/Mail.tsx
+++ src/screens/Mail/Mail.tsx
@@ -10,33 +10,44 @@ const Mail: NextPage<EmailProps> = (props) => {
   const { url, name } = props
   return (
     // <Html>
-    <Body>
-      <Tailwind>
-        <Section>
+    <Tailwind>
+      <Body className="w-[656px] m-auto flex flex-col justify-center height-[100vh]">
+        <Section className="p-6">
           <Img src="/assets/imgs/logo_header.png" />
         </Section>
-        <Section>
+        <Section className="p-6">
           <Text>Hi [{name}]</Text>
           <Text>
-            In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
-            demonstrate the visual form of a document or a typeface without relying on meaningful
-            content. Lorem ipsum may be used as a placeholder before final
+            We have received a request to reset the password for your account. If you did not make
+            this request, please ignore this email and your password will not be changed. To reset
+            your password, please click the button below:
           </Text>
-          <Button href={url}>Button</Button>
-          <Text>
+          <Button
+            href={url}
+            className="w-[154px] bg-[#3E19A3] text-center py-[14px] text-white rounded-[6px]"
+          >
+            Button
+          </Button>
+          <Text className="my-6">
             If you cannot click on the button, please use this link to access the password reset
             page:
-            <Link href="https://kpi-master.com/reset-password/abcxyz">
-              https://kpi-master.com/reset-password/abcxyz
-            </Link>
+            <Link href={url}>{url}</Link>
           </Text>
-          <Text>Thanks</Text>
-          <Text>The team</Text>
+          <Text>Thanks, The team</Text>
         </Section>
-      </Tailwind>
-    </Body>
+      </Body>
+    </Tailwind>
     // </Html>
   )
 }
 
 export { Mail }
+
+const h1 = {
+  color: '#333',
+  fontFamily: "Noto Sans', 'sans-serif",
+  fontSize: '24px',
+  fontWeight: 'bold',
+  margin: '40px 0',
+  padding: '0',
+}
