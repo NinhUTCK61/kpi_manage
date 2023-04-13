c5af7a2aa15dba9d004e0771856b8139c47aa96e
tranthinh201 <console.namee@gmail.com>
2023-04-13 17:24:32 +0700

    feat(KM-14): add client success page reset password

---
+++ src/screens/ResetPassword/FromResetPassword.tsx
@@ -0,0 +1,88 @@
+import { greyScale } from '@/common/theme'
+import { Input } from '@/components/Form/Input'
+import { LayoutUnAuth } from '@/components/Layout'
+import { PasswordStateValidation } from '@/components/PasswordStateValidation'
+import { ResetPasswordSchema } from '@/libs/schema'
+import { Button, Typography } from '@mui/material'
+import { Stack } from '@mui/system'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import ArrowLeft from 'public/assets/svgs/arrow_left.svg'
+import Logo from 'public/assets/svgs/logo.svg'
+import { Control, useWatch } from 'react-hook-form'
+import { z } from 'zod'
+
+type FormResetPasswordTypes = {
+  control: Control<z.infer<typeof ResetPasswordSchema>>
+  handleSubmit(): void
+  isLoading: boolean
+}
+
+const FormResetPassword: React.FC<FormResetPasswordTypes> = ({
+  control,
+  handleSubmit,
+  isLoading,
+}) => {
+  const router = useRouter()
+  const password = useWatch({ control, name: 'password' })
+
+  const redirectSignIn = () => {
+    router.push('/sign-in')
+  }
+  return (
+    <LayoutUnAuth title="Reset Password">
+      <Stack
+        width={460}
+        direction="column"
+        margin="auto"
+        mt={10}
+        component="form"
+        onSubmit={handleSubmit}
+      >
+        <Stack alignItems="center" mb={4}>
+          <Image src={Logo} alt="logo" />
+
+          <Typography variant="h2" mt={1.5} mb={0.5}>
+            Reset Password
+          </Typography>
+
+          <Typography variant="body1" color={greyScale[600]} textAlign="center" width={507}>
+            Keep your account secure by changing your password regularly
+          </Typography>
+        </Stack>
+
+        <Stack width="100%" spacing={2}>
+          <Input
+            sx={{ width: 450 }}
+            control={control}
+            name="password"
+            label="New password"
+            type="password"
+            placeholder="enter your new password"
+          />
+
+          <PasswordStateValidation password={password} />
+
+          <Button
+            variant="contained"
+            disabled={isLoading}
+            sx={{ textTransform: 'capitalize', width: 450 }}
+            type="submit"
+          >
+            Submit
+          </Button>
+
+          <Stack py={1.5} spacing={0.5} justifyContent="center" alignItems="center" direction="row">
+            <Image src={ArrowLeft} alt="arrow-left" style={{ marginRight: '14px' }} />
+
+            <Typography variant="body2" sx={{ cursor: 'pointer' }} onClick={redirectSignIn}>
+              Back
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </LayoutUnAuth>
+  )
+}
+
+export { FormResetPassword }
--- src/screens/ResetPassword/ResetPassword.tsx
+++ src/screens/ResetPassword/ResetPassword.tsx
@@ -1,25 +1,19 @@
-import { greyScale } from '@/common/theme'
-import { Input } from '@/components/Form/Input'
 import { LayoutUnAuth } from '@/components/Layout'
-import { PasswordStateValidation } from '@/components/PasswordStateValidation'
 import { ResetPasswordSchema, ResetPasswordType } from '@/libs/schema'
 import { api } from '@/utils/api'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Typography } from '@mui/material'
-import { Stack } from '@mui/system'
 import { NextPage } from 'next'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { enqueueSnackbar } from 'notistack'
-import ArrowLeft from 'public/assets/svgs/arrow_left.svg'
-import Logo from 'public/assets/svgs/logo.svg'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { FormResetPassword } from './FromResetPassword'
+import { Success } from './Success'
 
 const ResetPassword: NextPage = () => {
   const router = useRouter()
-  const mutation = api.auth.resetPassword.useMutation()
+  const { mutate, isLoading, isSuccess } = api.auth.resetPassword.useMutation()
 
-  const { control, handleSubmit, watch } = useForm<ResetPasswordType>({
+  const { control, handleSubmit } = useForm<ResetPasswordType>({
     defaultValues: {
       password: '',
       token: '',
@@ -27,51 +21,27 @@ const ResetPassword: NextPage = () => {
     resolver: zodResolver(ResetPasswordSchema),
   })
 
-  const password = watch('password')
-
-  const redirectBack = () => {
-    router.back()
-  }
-
-  const { token } = router.query as { token: string }
   const onSubmit: SubmitHandler<ResetPasswordType> = (data) => {
-    mutation.mutate(
+    const { token } = router.query as { token: string }
+    mutate(
       {
         password: data.password,
         token,
       },
       {
         onError(error) {
-          if (typeof error === 'object' && error.message) {
-            try {
-              const parsedError = JSON.parse(error.message)
-              if (Array.isArray(parsedError)) {
-                parsedError.forEach((item) => {
-                  enqueueSnackbar(`${item.message}`, {
-                    variant: 'error',
-                  })
-                })
-              } else {
-                enqueueSnackbar(`${parsedError}`, {
-                  variant: 'error',
-                })
-              }
-            } catch {
-              enqueueSnackbar(`${error.message}`, {
-                variant: 'error',
-              })
-            }
-          } else if (typeof error === 'string') {
-            enqueueSnackbar(`${error}`, {
+          if (error.data?.zodError) {
+            const errorMes = JSON.parse(error.message)[0].message
+            enqueueSnackbar(`${errorMes}`, {
               variant: 'error',
             })
+
+            return
           }
-        },
-        onSuccess() {
-          enqueueSnackbar('Change password success!', {
-            variant: 'success',
+
+          enqueueSnackbar(`${error.message}`, {
+            variant: 'error',
           })
-          router.push('/sign-in')
         },
       },
     )
@@ -79,47 +49,15 @@ const ResetPassword: NextPage = () => {
 
   return (
     <LayoutUnAuth title="Reset Password">
-      <Stack width={460} direction="column" margin="auto" mt={10}>
-        <Stack alignItems="center" mb={4}>
-          <Image src={Logo} alt="logo" />
+      {!isSuccess && (
+        <FormResetPassword
+          isLoading={isLoading}
+          control={control}
+          handleSubmit={handleSubmit(onSubmit)}
+        />
+      )}
 
-          <Typography variant="h2" mt={1.5} mb={0.5}>
-            Reset Password
-          </Typography>
-
-          <Typography variant="body1" color={greyScale[600]} textAlign="center" width={507}>
-            Keep your account secure by changing your password regularly
-          </Typography>
-        </Stack>
-
-        <Stack width="100%" spacing={2}>
-          <Input
-            sx={{ width: 450 }}
-            control={control}
-            name="password"
-            label="New password"
-            type="password"
-            placeholder="enter your new password"
-          />
-          <PasswordStateValidation password={password} />
-
-          <Button
-            variant="contained"
-            onClick={handleSubmit(onSubmit)}
-            sx={{ textTransform: 'capitalize', width: 450 }}
-          >
-            Submit
-          </Button>
-
-          <Stack py={1.5} spacing={0.5} justifyContent="center" alignItems="center" direction="row">
-            <Image src={ArrowLeft} alt="arrow-left" style={{ marginRight: '14px' }} />
-
-            <Typography variant="body2" sx={{ cursor: 'pointer' }} onClick={redirectBack}>
-              Back
-            </Typography>
-          </Stack>
-        </Stack>
-      </Stack>
+      {isSuccess && <Success />}
     </LayoutUnAuth>
   )
 }
