57dc7cd3c4e8c0f607b96f119ec890000e5d4cf9
thinh201 <thinh221201@gmail.com>
2023-04-18 14:40:33 +0700

    feat(KM-32): change schema api update-template

--- src/libs/schema/template.ts
+++ src/libs/schema/template.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const UpdateTemplateSchema = z.object({
-  name: z.string().min(8).max(255).optional(),
+  name: z.string().min(1).max(255).optional(),
   imageUrl: z.string().optional(),
   id: z.string(),
 })
