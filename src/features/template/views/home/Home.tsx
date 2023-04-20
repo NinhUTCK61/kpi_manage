fdd32058c9d7e71008460779a64948a7d2c15d83
Nguyen Van Hieu <hieunv1@solashi.com>
2023-04-20 16:42:34 +0700

    chore : connect api view home

--- src/features/template/views/home/Home.tsx
+++ src/features/template/views/home/Home.tsx
@@ -1,97 +1,55 @@
+import { api } from '@/libs/api'
 import { base } from '@/libs/config/theme'
 import { useModalState } from '@/libs/hooks'
-import type { TemplateDataSchema } from '@/libs/schema'
 import { DialogAction, DialogThumbnail, Layout, Menu, MenuItem } from '@/libs/shared/components'
 import { DialogActionType } from '@/libs/shared/types/utils'
 import { Button, Grid, Stack, Typography } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { enqueueSnackbar } from 'notistack'
 import ArrowDownIcon from 'public/assets/svgs/arrow_down.svg'
 import ArrowLeftIcon from 'public/assets/svgs/arrow_left_account.svg'
 import AddIcon from 'public/assets/svgs/plus.svg'
 import { useState } from 'react'
-import { z } from 'zod'
-import { FileAction, StatusTemplate } from '../../types/template'
+import { FileAction } from '../../types/template'
 import { ButtonCreate, TemplateItem } from './components'
 
 const Home = () => {
+  const router = useRouter()
   const { t } = useTranslation('home')
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
-  const [status, setStatus] = useState<StatusTemplate>(StatusTemplate.all)
-  const menu = [
-    {
-      id: StatusTemplate.all,
-      title: t('all_file'),
-      handle: handleClose,
-    },
-    {
-      id: StatusTemplate.deleted,
-      title: t('deleted_file'),
-      handle: handleClose,
-    },
-  ]
-
-  const [fakeData, setFakeData] = useState<z.infer<typeof TemplateDataSchema>[] | undefined>(
-    Array(8)
-      .fill(0)
-      .map((e, index) => {
-        return {
-          template_id: `${index}`,
-          name: 'Template 1',
-          root_note_id: '1',
-          delete_at: '',
-          public_url: '',
-          image_url: '',
-          is_owner: true,
-          is_favorite: true,
-          can_edit: true,
-          created_at: new Date(),
-          updated_at: new Date(),
-          deleted_at: null,
-        }
-      }),
-  )
-
-  const [fakeDataDelete, setFakeDataDelete] = useState<
-    z.infer<typeof TemplateDataSchema>[] | undefined
-  >(
-    Array(3)
-      .fill(0)
-      .map((e, index) => {
-        return {
-          template_id: `${index}`,
-          name: 'Template 1',
-          root_note_id: `${index}`,
-          delete_at: '',
-          public_url: '',
-          image_url: '',
-          is_owner: true,
-          is_favorite: true,
-          can_edit: true,
-          created_at: new Date(),
-          updated_at: new Date(),
-          deleted_at: new Date(),
-        }
-      }),
-  )
-
+  const [nodeId, setNodeId] = useState<string>()
   const [action, setAction] = useState<Exclude<FileAction, FileAction.UpdateThumbnail> | null>(null)
+  const [isTrash, setIsTrash] = useState<boolean>(false)
+  const openMenu = Boolean(anchorEl)
   const {
     isOpen: isOpenThumbnail,
     onOpen: openDialogThumbnail,
     onClose: closeDialogThumbnail,
   } = useModalState()
+  
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
 
-  const [_nodeId, setNodeId] = useState<string>()
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const menu = [
+    {
+      title: t('all_file'),
+      handle: () => setIsTrash(false),
+    },
+    {
+      title: t('deleted_file'),
+      handle: () => setIsTrash(true),
+    },
+  ]
+
+
+
 
   const handleFileAction = (id: string, type: FileAction) => {
     setNodeId(id)
@@ -100,28 +58,6 @@ const Home = () => {
     } else {
       setAction(type)
     }
-  }
-
-  const handleConfirmDelete = () => {
-    fakeData?.pop()
-    const _fakeData = fakeData
-    setFakeData(_fakeData)
-    enqueueSnackbar('', {
-      variant: 'success',
-      description: t('description_delete_success') as string,
-    })
-    setAction(null)
-  }
-
-  const handleConfirmDeletePer = () => {
-    fakeDataDelete?.pop()
-    const _fakeDataDelete = fakeDataDelete
-    setFakeDataDelete(_fakeDataDelete)
-    enqueueSnackbar('', {
-      variant: 'success',
-      description: t('description_delete_success') as string,
-    })
-    setAction(null)
   }
 
   const handleConfirmThumbnail = () => {
@@ -136,29 +72,108 @@ const Home = () => {
     [FileAction.Delete]: {
       title: t('delete_file'),
       description: t('detail_dialog_delete'),
-      handleConfirm: handleConfirmDelete,
+      handleConfirm: () => handleConfirmDelete(),
       type: 'delete',
       textSubmit: t('delete'),
     },
     [FileAction.Restore]: {
       title: t('restore'),
       description: t('detail_dialog_restore'),
-      handleConfirm: handleConfirmDelete,
+      handleConfirm: () => handleConfirmRestore(),
       type: 'warning',
       textSubmit: t('restore'),
     },
     [FileAction.DeletePermanently]: {
       title: t('permanently_delete'),
       description: t('detail_dialog_delete_per'),
-      handleConfirm: handleConfirmDeletePer,
-      type: 'warning',
+      handleConfirm: () => handleConfirmDelete(true),
+      type: 'delete',
       textSubmit: t('permanently_delete'),
     },
   }
 
+  const { data, refetch } = api.template.getListTemplate.useQuery({
+    isTrash: isTrash,
+  })
+  const { mutate: create } = api.template.createTemplate.useMutation()
+
+  const { mutate: deleteTemplate } = api.template.deleteTemplate.useMutation()
+
+  const { mutate: restore } = api.template.restoreTemplate.useMutation()
+
+  const handleCreateTemplate = () => {
+    create(
+      {},
+      {
+        onSuccess: (data) => {
+          router.push(`/template/${data.id}`)
+        },
+        onError: (err) => {
+          const error = String(err.message)
+          const description = t(error)
+          enqueueSnackbar(t('create_failed'), { variant: 'error', description })
+        },
+      },
+    )
+  }
+
+  const handleConfirmDelete = (is_permanently?: boolean) => {
+    deleteTemplate(
+      {
+        id: String(nodeId),
+        is_permanently,
+      },
+      {
+        onSuccess: () => {
+          enqueueSnackbar('', {
+            variant: 'success',
+            description: t('description_delete_success') as string,
+          })
+          setAction(null)
+          refetch()
+        },
+        onError: (err) => {
+          const error = String(err.message)
+          const description = t(error)
+          enqueueSnackbar(t(is_permanently ? 'permanently_delete_failed' : 'delete_failed'), {
+            variant: 'error',
+            description,
+          })
+        },
+      },
+    )
+  }
+
+  const handleConfirmRestore = () => {
+    restore(
+      {
+        id: String(nodeId),
+      },
+      {
+        onSuccess: () => {
+          enqueueSnackbar(t('restore_success'), {
+            variant: 'success',
+            description: t('description_restore_success') as string,
+          })
+          setAction(null)
+          refetch()
+        },
+        onError: (err) => {
+          const error = String(err.message)
+          const description = t(error)
+          enqueueSnackbar(t('restore_failed'), { variant: 'error', description })
+        },
+      },
+    )
+  }
+
   return (
     <Layout title={t('seo_title')}>
-      <ButtonCreate variant="contained" disableRipple startIcon={<Image src={AddIcon} alt="add" />}>
+      <ButtonCreate
+        variant="contained"
+        startIcon={<Image src={AddIcon} alt="add" />}
+        onClick={handleCreateTemplate}
+      >
         {t('create')}
       </ButtonCreate>
       <Stack direction="row" justifyContent="space-between" mb={1.5}>
@@ -166,20 +181,21 @@ const Home = () => {
           {t('my_files')}
         </Typography>
         <Button
-          aria-controls={open ? 'status-menu' : undefined}
+          aria-controls={openMenu ? 'status-menu' : undefined}
           aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
+          aria-expanded={openMenu ? 'true' : undefined}
+          disableRipple
           onClick={handleClick}
           sx={{ color: base.black }}
-          startIcon={<Image src={open ? ArrowLeftIcon : ArrowDownIcon} alt="down" />}
+          startIcon={<Image src={openMenu ? ArrowLeftIcon : ArrowDownIcon} alt="down" />}
         >
-          {menu.find((e) => e.id === status)?.title}
+          {isTrash ? t('deleted_file') : t('all_file')}
         </Button>
 
         <Menu
           anchorEl={anchorEl}
           id="status-menu"
-          open={open}
+          open={openMenu}
           onClose={handleClose}
           onClick={handleClose}
           transformOrigin={{ horizontal: 'right', vertical: 'top' }}
@@ -192,20 +208,22 @@ const Home = () => {
           elevation={1}
         >
           {menu.map((item) => (
-            <MenuItem key={item.title} onClick={() => setStatus(item.id)}>
+            <MenuItem key={item.title} onClick={item.handle}>
               {item.title}
             </MenuItem>
           ))}
         </Menu>
       </Stack>
       <Grid container rowSpacing={4} spacing={2} columns={{ md: 12, xl: 15 }}>
-        {((status === StatusTemplate.all ? fakeData : fakeDataDelete) || []).map(
-          (template, index) => (
-            <Grid item lg={3} md={4} sm={5} xs={12} key={index}>
-              <TemplateItem template={template} handleFileAction={handleFileAction} />
-            </Grid>
-          ),
-        )}
+        {(data || []).map((template, index) => (
+          <Grid item key={index} xl="auto" lg={3} md={4} sm={5} xs={12}>
+            <TemplateItem
+              template={template}
+              handleFileAction={handleFileAction}
+              refetch={refetch}
+            />
+          </Grid>
+        ))}
       </Grid>
       {action && (
         <DialogAction
