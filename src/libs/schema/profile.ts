f0f6af61f3f1bc306682292cbca6e631c9387e2e
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-05-09 16:16:57 +0700

    fix(KM-91): format schem input api

--- src/libs/schema/profile.ts
+++ src/libs/schema/profile.ts
@@ -1,13 +1,8 @@
 import { UserSchema } from 'prisma/generated/zod'
 import { z } from 'zod'
 
-export const UpdateProfileInputSchema = UserSchema.omit({
-  date_of_birth: true,
-  password: true,
+export const UpdateProfileInputSchema = UserSchema.partial().required({
   id: true,
-  created_at: true,
-  updated_at: true,
-  emailVerified: true,
 })
 
 export type UpdateProfileType = z.infer<typeof UpdateProfileInputSchema>
