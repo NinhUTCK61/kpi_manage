7bb9096eb7192bc2b2316e96f6868e06dfdaf14b
hoangtrung <trungnh@solashi.com>
2023-05-12 13:07:47 +0700

    refactor: test

--- src/libs/react-flow/__test__/math.test.ts
+++ src/libs/react-flow/__test__/math.test.ts
@@ -1,13 +1,13 @@
 import { describe, expect, test } from 'vitest'
 import { getSlugFromInputValue } from '../helper/expression'
 
-describe('Node Hierarchy', () => {
-  test('Convert expression to array key', () => {
+describe('Test expression to slugs', () => {
+  test('(A1 + A2)*A3', () => {
     const result = getSlugFromInputValue('(A1 + A2)*A3')
     expect(result).toStrictEqual(['A1', 'A2', 'A3'])
   })
 
-  test('Convert expression to array key', () => {
+  test('A1 + (A2 *A3 +1', () => {
     const result = getSlugFromInputValue('A1 + (A2 *A3 +1')
     expect(result).toStrictEqual(['A1', 'A2', 'A3'])
   })
@@ -22,8 +22,8 @@ describe('Node Hierarchy', () => {
     expect(result).toStrictEqual(['A111'])
   })
 
-  test('Convert expression to array key', () => {
-    const result = getSlugFromInputValue('A_1 / A^3 ')
-    expect(result).toStrictEqual(['A_1', 'A^3'])
+  test('A1 + (A2)^3', () => {
+    const result = getSlugFromInputValue('A1 + (A2)^3')
+    expect(result).toStrictEqual(['A1', 'A2'])
   })
 })
