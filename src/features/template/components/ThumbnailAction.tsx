bb2e91fee4818e8280ad559038f28a7f38d6e521
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-05-13 15:06:52 +0700

    refactor(KM-120): update single function validate

--- src/features/template/components/ThumbnailAction.tsx
+++ src/features/template/components/ThumbnailAction.tsx
@@ -19,19 +19,37 @@ const ThumbnailAction: React.FC<ThumbnailActionTypes> = ({
   const { t } = useTranslation(['home'])
   const [image, setImage] = useState<File[] | null>()
   const onSelectImage = (_acceptedFiles: File[]) => {
+    handleValidateAllCase(_acceptedFiles)
+  }
+
+  function handleValidateFormatImage(_acceptedFiles: File[]) {
     if (!_acceptedFiles.length) {
       enqueueSnackbar(t('incorrect_upload_image'), {
         variant: 'error',
       })
-      return
-    } else if (_acceptedFiles[0]?.size && Math.floor(_acceptedFiles[0]?.size / (1024 * 1024)) > 3) {
+      return false
+    }
+    return true
+  }
+
+  function handleValidateSizeImage(_acceptedFiles: File[]) {
+    if (_acceptedFiles[0]?.size && Math.floor(_acceptedFiles[0]?.size / (1024 * 1024)) > 3) {
       enqueueSnackbar(t('error_size_image_upload'), {
         variant: 'error',
       })
-      return
-    } else {
-      setImage(_acceptedFiles)
+      return false
     }
+    return true
+  }
+
+  function handleValidateAllCase(_acceptedFiles: File[]) {
+    handleValidateFormatImage(_acceptedFiles) && handleValidateSizeImage(_acceptedFiles)
+      ? saveImage(_acceptedFiles)
+      : onCloseModal()
+  }
+
+  function saveImage(_acceptedFiles: File[]) {
+    setImage(_acceptedFiles)
     onClose()
   }
 
