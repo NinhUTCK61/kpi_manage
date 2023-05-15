9155790c735ab7c343a6b81ce8e7288d71e3a7cb
hoangtrung <trungnh@solashi.com>
2023-05-15 14:40:33 +0700

    fix: delete node update db

--- src/libs/react-flow/components/KPINode/hooks/useNodeDeleteMutation.ts
+++ src/libs/react-flow/components/KPINode/hooks/useNodeDeleteMutation.ts
@@ -1,14 +1,23 @@
 import { api } from '@/libs/api'
 import { useRFStore } from '@/libs/react-flow/hooks'
-import { ReactFlowNodeOutputType, ReactFlowOutputEdge } from '@/libs/schema/node'
+import { ReactFlowKPINode } from '@/libs/react-flow/types'
+import {
+  ReactFlowKPINodeOutputType,
+  ReactFlowNodeOutputType,
+  ReactFlowOutputEdge,
+} from '@/libs/schema/node'
+import { intersectionBy } from 'lodash'
 import { useTranslation } from 'next-i18next'
 import { enqueueSnackbar } from 'notistack'
+import { filterKpiNodes, getDifferenceNodesByPosition } from '../utils'
 
 const useNodeDeleteMutation = () => {
   const removeNode = useRFStore((state) => state.removeNode)
   const templateId = useRFStore((state) => state.templateId)
   const utils = api.useContext()
   const { t } = useTranslation('common')
+
+  const { mutate: bulkUpdate } = api.node.bulkUpdate.useMutation()
 
   const mutation = api.node.delete.useMutation({
     async onMutate(variables) {
@@ -23,7 +32,7 @@ const useNodeDeleteMutation = () => {
         },
       )
 
-      return { prevData, templateId }
+      return { prevData, templateId, nodes, edges }
     },
     onError(err, _, ctx) {
       enqueueSnackbar(t('error.internal_server_error'), {
@@ -31,6 +40,22 @@ const useNodeDeleteMutation = () => {
       })
 
       utils.node.list.setData({ template_id: ctx?.templateId as string }, ctx?.prevData)
+    },
+    onSuccess(_, __, ctx) {
+      const newNodes = filterKpiNodes<ReactFlowKPINode>(ctx?.nodes || [])
+      const oldAllNodes = ctx?.prevData?.nodes || []
+      const oldNodeNeedUpdated = filterKpiNodes<ReactFlowKPINodeOutputType>(
+        intersectionBy(oldAllNodes, newNodes, 'id'),
+      )
+
+      if (oldNodeNeedUpdated.length) {
+        // get difference nodes by position
+        const diff = getDifferenceNodesByPosition(newNodes, oldNodeNeedUpdated)
+
+        if (diff.length) {
+          bulkUpdate(diff.map((n) => n.data))
+        }
+      }
     },
     onSettled() {
       utils.node.list.invalidate()
