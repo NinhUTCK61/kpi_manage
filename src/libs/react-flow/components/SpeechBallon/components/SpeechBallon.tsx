823c265ad8c087ac806dd9412cf4ea3a9f666c07
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-05-16 16:43:42 +0700

    refactor(KM-72): up to date develop

---
+++ src/libs/react-flow/components/SpeechBallon/components/SpeechBallon.tsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+
+import { OptionShape } from './OptionShape'
+
+const SpeechBallon: React.FC = () => {
+  return (
+    <Stack component="form" spacing={0.5}>
+      <OptionShape />
+    </Stack>
+  )
+}
+
+export { SpeechBallon }
