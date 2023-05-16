9fe06928a3875faf974eaaaec3ed6f4fa1b1a31f
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-05-16 09:49:20 +0700

    refactor(KM-108): rebase code

--- src/libs/react-flow/helper/expression.ts
+++ src/libs/react-flow/helper/expression.ts
@@ -1,9 +1,25 @@
 import * as math from 'mathjs'
 
+const sliceKeyInsideSpace = (inputString: string, cursorPosition: number) => {
+  const spaceBeforeIndex = inputString.lastIndexOf(' ', cursorPosition - 1)
+  const spaceAfterIndex = inputString.indexOf(' ', cursorPosition)
+  const startIndex = spaceBeforeIndex + 1
+  const endIndex = spaceAfterIndex !== -1 ? spaceAfterIndex : inputString.length
+  const resultString = inputString.substring(startIndex, endIndex)
+  return resultString.replace(/[^a-zA-Z]/g, ' ')
+}
+
+export const charNearCursorKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const inputElement = e.target as HTMLInputElement
+  const selectionStart = inputElement.selectionStart
+  if (selectionStart && selectionStart >= 1 && selectionStart <= inputElement.value.length) {
+    sliceKeyInsideSpace(inputElement.value.replace(/[^a-zA-Z0-9]/g, ' '), selectionStart)
+  }
+}
+
 export const getSlugFromInputValue = (inputValue: string) => {
   const slugs: string[] = []
   const nodeParse = math.parse(inputValue)
-
   nodeParse.traverse(function (node: math.MathNode) {
     if (node.type === 'SymbolNode') {
       slugs.push((node as math.SymbolNode).name)
