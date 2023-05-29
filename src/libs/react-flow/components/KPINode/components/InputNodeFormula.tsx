b7fd695fd5847a4cc18fb2413e53cf94252484b3
Nguyen Van Hieu <hieunv1@solashi.com>
2023-05-29 10:09:09 +0700

    chore: remove comment

--- src/libs/react-flow/components/KPINode/components/InputNodeFormula.tsx
+++ src/libs/react-flow/components/KPINode/components/InputNodeFormula.tsx
@@ -1,22 +1,11 @@
 import { BaseInputProps } from '@/libs/shared/components'
 import { InputBaseProps, styled } from '@mui/material'
-import { forwardRef } from 'react'
 import type { FieldValues } from 'react-hook-form'
 import { useController } from 'react-hook-form'
 import { IMaskInput } from 'react-imask'
 import { InputControlNode } from './InputControlNode'
 
 type InputNodeProps<T extends FieldValues> = BaseInputProps<T> & InputBaseProps
-
-type CustomProps = {
-  onChange: (event: { target: { name: string; value: string } }) => void
-  name: string
-}
-
-const InputMaskCustom = forwardRef<HTMLElement, CustomProps>(function TextMaskCustom(props, ref) {
-  const { onChange, ...other } = props
-  return <IMaskInput {...other} />
-})
 
 function InputNodeFormula<T extends FieldValues>({
   name,
@@ -39,12 +28,12 @@ function InputNodeFormula<T extends FieldValues>({
           {
             mask: Number,
             thousandsSeparator: ',',
-            scale: 3, // digits after point, 0 for integers
-            signed: false, // disallow negative
-            padFractionalZeros: false, // if true, then pads zeros at end to the length of scale
-            normalizeZeros: true, // appends or removes zeros at ends
-            radix: '.', // fractional delimiter
-            mapToRadix: ['.'], // symbols to process as radix
+            scale: 3,
+            signed: false,
+            padFractionalZeros: false,
+            normalizeZeros: true,
+            radix: '.',
+            mapToRadix: ['.'],
           },
           {
             mask: /^=[0-9a-zA-Z+$!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{0,999}$/i,
@@ -77,16 +66,5 @@ const InputStyled = styled(IMaskInput)(({ theme }) => ({
     outline: 'none',
   },
 }))
-
-// const InputStyled = styled(InputBase)(({ theme }) => ({
-//   '& .MuiInputBase-input': {
-//     width: 140,
-//     fontSize: 15,
-//     lineHeight: '22px',
-//     padding: 0,
-//     color: theme.palette.base.black,
-//   },
-//   marginBottom: 4,
-// }))
 
 export { InputNodeFormula }
