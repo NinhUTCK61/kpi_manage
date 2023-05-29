5d22552aa35ab2634752f3cb1e6d4bee9a3beb11
Nguyen Van Hieu <hieunv1@solashi.com>
2023-05-29 10:45:30 +0700

    chore: update unmark

--- src/libs/react-flow/components/KPINode/components/InputNodeFormula.tsx
+++ src/libs/react-flow/components/KPINode/components/InputNodeFormula.tsx
@@ -40,7 +40,7 @@ function InputNodeFormula<T extends FieldValues>({
             mask: /^=[0-9a-zA-Z+$!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{0,999}$/i,
           },
         ]}
-        unmask={true}
+        unmask
         onAccept={(value: string) => {
           onChange(value)
         }}
