76baeee42437d85f3996e585fc5787e707b2e3e8
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-06-05 11:11:30 +0700

    refactor(KM-176): change the svg element to props

--- src/features/node/views/create/components/style.tsx
+++ src/features/node/views/create/components/style.tsx
@@ -1,23 +1,15 @@
-import { SvgIcon } from '@mui/material'
+import { SvgIcon, SvgIconProps } from '@mui/material'
 
-type PropsStyle = {
-  style: {
-    fill: string
-    stroke: string
-  }
-}
-
-function Circular({ style }: PropsStyle) {
+function Circular(style: SvgIconProps) {
   return (
     <SvgIcon
-      sx={{ color: style.fill }}
+      {...style}
       viewBox="3.887 1.745 31.155 20.705"
       width="28"
       height="25"
       xmlns="http://www.w3.org/2000/svg"
     >
       <rect
-        stroke={style.stroke}
         x="4.863"
         y="2.693"
         width="29.344"
@@ -30,46 +22,38 @@ function Circular({ style }: PropsStyle) {
   )
 }
 
-function SquareShape({ style }: PropsStyle) {
+function SquareShape(style: SvgIconProps) {
   return (
     <SvgIcon
-      sx={{ color: style.fill }}
+      {...style}
       width="25"
       height="25"
       viewBox="0 0 25 24"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect stroke={style.stroke} x="1.25" y="0.75" width="22.5" height="22.5" strokeWidth="1.5" />
+      <rect x="1.25" y="0.75" width="22.5" height="22.5" strokeWidth="1.5" />
     </SvgIcon>
   )
 }
 
-function RoundSquare({ style }: PropsStyle) {
+function RoundSquare(style: SvgIconProps) {
   return (
     <SvgIcon
-      sx={{ color: style.fill }}
+      {...style}
       width="25"
       height="25"
       viewBox="0 0 24 19"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect
-        stroke={style.stroke}
-        x="0.75"
-        y="0.75"
-        width="22.5"
-        height="17.5"
-        rx="6.25"
-        strokeWidth="1.5"
-      />
+      <rect x="0.75" y="0.75" width="22.5" height="17.5" rx="6.25" strokeWidth="1.5" />
     </SvgIcon>
   )
 }
 
-function MediumRoundSquare({ style }: PropsStyle) {
+function MediumRoundSquare(style: SvgIconProps) {
   return (
     <SvgIcon
-      sx={{ color: style.fill }}
+      {...style}
       width="25"
       height="25"
       viewBox="0 0 24 24"
