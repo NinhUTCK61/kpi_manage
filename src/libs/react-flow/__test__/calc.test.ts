a122cc43f1718ef90ed8e8fb1320c8abb02c8380
hoangtrung <trungnh@solashi.com>
2023-06-05 17:19:22 +0700

    feat: update calc stack

--- src/libs/react-flow/__test__/calc.test.ts
+++ src/libs/react-flow/__test__/calc.test.ts
@@ -4,7 +4,7 @@ import { DEFAULT_NODE_ATTRIBUTES } from '../constant'
 import { getSlugFromInputValue } from '../helper/expression'
 
 export const generateCalculatorStack = (nodes: ReactFlowKPINode[]) => {
-  const valid = new Map()
+  const valid = new Map<string, string[] | boolean>()
   const stack: string[] = []
 
   nodes.forEach((node) => {
@@ -17,6 +17,20 @@ export const generateCalculatorStack = (nodes: ReactFlowKPINode[]) => {
     const expressionArr = getSlugFromInputValue(input_value as string)
     const expressionStack = [...expressionArr, slug]
 
+    // Kiểm tra sự phụ thuộc giữa các toán hạng so với slug
+    // Nếu tồn tại => lỗi
+    // Nếu không tồn tại => thêm vào stack
+
+    // validate
+    expressionArr.forEach((_slug) => {
+      const exists = valid.get(_slug)
+      if (exists) {
+        if (Array.isArray(exists) && exists.includes(slug)) {
+          throw new Error(`Cyclic dependency detected: ${_slug} -> ${slug}`)
+        }
+      }
+    })
+
     valid.set(slug, expressionArr)
     // validate expression
     if (!stack.length) {
@@ -24,18 +38,22 @@ export const generateCalculatorStack = (nodes: ReactFlowKPINode[]) => {
       return
     }
 
-    let maxIndex = 0
     expressionArr.forEach((_slug) => {
       if (!stack.includes(_slug)) {
         stack.unshift(_slug)
         valid.set(_slug, true)
       }
+    })
 
-      const indexSlug = stack.indexOf(slug)
+    let maxIndex = 0
+    expressionArr.forEach((_slug) => {
+      const indexSlug = stack.indexOf(_slug)
       if (indexSlug > maxIndex) {
         maxIndex = indexSlug
       }
     })
+
+    console.log(111, maxIndex, expressionArr, slug, stack)
 
     // check if slug exists in stack to validate and move to the right position of maxIndex
     // else add after maxIndex
@@ -45,7 +63,7 @@ export const generateCalculatorStack = (nodes: ReactFlowKPINode[]) => {
       stack.splice(indexSlug, 1)
     }
 
-    stack.splice(maxIndex, 0, slug)
+    stack.splice(maxIndex + 1, 0, slug)
   })
 
   return stack
@@ -78,12 +96,29 @@ const nodes: ReactFlowKPINode[] = [
     },
     position: { x: 0, y: 0 },
   },
+  {
+    id: 'E',
+    data: {
+      ...DEFAULT_NODE_ATTRIBUTES,
+      is_saved: true,
+      slug: 'E',
+      is_formula: true,
+      input_value: '=A+F',
+      id: 'E',
+      parent_node_id: null,
+    },
+    position: { x: 0, y: 0 },
+  },
 ]
 
 describe('generateCalculatorStack', () => {
   test('should return correct stack', () => {
-    const stack = generateCalculatorStack(nodes)
-    console.log(stack)
-    expect(stack).toEqual(['D', 'B', 'C', 'A'])
+    try {
+      const stack = generateCalculatorStack(nodes)
+      console.log(stack)
+      expect(stack).toEqual(['D', 'B', 'C', 'A'])
+    } catch (error) {
+      console.log(error)
+    }
   })
 })
--- src/libs/react-flow/components/KPINode/helper/calculator.ts
+++ src/libs/react-flow/components/KPINode/helper/calculator.ts
@@ -2,7 +2,7 @@ import { getSlugFromInputValue } from '@/libs/react-flow/helper/expression'
 import { ReactFlowKPINode } from '@/libs/react-flow/types'
 
 export const generateCalculatorStack = (nodes: ReactFlowKPINode[]) => {
-  const valid = new Map()
+  const valid = new Map<string, string[] | boolean>()
   const stack: string[] = []
 
   nodes.forEach((node) => {
@@ -15,24 +15,51 @@ export const generateCalculatorStack = (nodes: ReactFlowKPINode[]) => {
     const expressionArr = getSlugFromInputValue(input_value as string)
     const expressionStack = [...expressionArr, slug]
 
+    // Kiểm tra sự phụ thuộc giữa các toán hạng so với slug
+    // Nếu tồn tại => lỗi
+    // Nếu không tồn tại => thêm vào stack
+
+    // validate
+    expressionArr.forEach((_slug) => {
+      const exists = valid.get(_slug)
+      if (exists) {
+        if (Array.isArray(exists) && exists.includes(slug)) {
+          throw new Error(`Cyclic dependency detected: ${_slug} -> ${slug}`)
+        }
+      }
+    })
+
     valid.set(slug, expressionArr)
     // validate expression
     if (!stack.length) {
       stack.push(...expressionStack)
+      return
     }
 
-    let maxIndex = 0
     expressionArr.forEach((_slug) => {
       if (!stack.includes(_slug)) {
         stack.unshift(_slug)
         valid.set(_slug, true)
       }
+    })
 
-      const indexSlug = stack.indexOf(slug)
+    let maxIndex = 0
+    expressionArr.forEach((_slug) => {
+      const indexSlug = stack.indexOf(_slug)
       if (indexSlug > maxIndex) {
         maxIndex = indexSlug
       }
     })
+
+    console.log(111, maxIndex, expressionArr, slug, stack)
+
+    // check if slug exists in stack to validate and move to the right position of maxIndex
+    // else add after maxIndex
+
+    const indexSlug = stack.indexOf(slug)
+    if (indexSlug !== -1) {
+      stack.splice(indexSlug, 1)
+    }
 
     stack.splice(maxIndex + 1, 0, slug)
   })
