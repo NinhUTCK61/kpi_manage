e80949ab5965e2ab0ce054d0055f05ca18015459
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-06-05 10:47:07 +0700

    style(KM-176): fill color on the shape

--- src/features/node/views/create/components/style.tsx
+++ src/features/node/views/create/components/style.tsx
@@ -1,63 +1,90 @@
-function Circular() {
+import { SvgIcon } from '@mui/material'
+
+type PropsStyle = {
+  style: {
+    fill: string
+    stroke: string
+  }
+}
+
+function Circular({ style }: PropsStyle) {
   return (
-    <svg
+    <SvgIcon
+      sx={{ color: style.fill }}
       viewBox="3.887 1.745 31.155 20.705"
       width="28"
       height="25"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <rect
+        stroke={style.stroke}
         x="4.863"
         y="2.693"
         width="29.344"
         height="18.839"
-        stroke="black"
-        strokeWidth="1.5"
+        strokeWidth="1.9"
         rx="9.42"
         ry="9.42"
       />
-    </svg>
+    </SvgIcon>
   )
 }
 
-function SquareShape() {
+function SquareShape({ style }: PropsStyle) {
   return (
-    <svg width="25" height="25" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="1.25" y="0.75" width="22.5" height="22.5" stroke="black" strokeWidth="1.5" />
-    </svg>
+    <SvgIcon
+      sx={{ color: style.fill }}
+      width="25"
+      height="25"
+      viewBox="0 0 25 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect stroke={style.stroke} x="1.25" y="0.75" width="22.5" height="22.5" strokeWidth="1.5" />
+    </SvgIcon>
   )
 }
 
-function RoundSquare() {
+function RoundSquare({ style }: PropsStyle) {
   return (
-    <svg width="25" height="25" viewBox="0 0 24 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <SvgIcon
+      sx={{ color: style.fill }}
+      width="25"
+      height="25"
+      viewBox="0 0 24 19"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <rect
+        stroke={style.stroke}
         x="0.75"
         y="0.75"
         width="22.5"
         height="17.5"
         rx="6.25"
-        stroke="black"
         strokeWidth="1.5"
       />
-    </svg>
+    </SvgIcon>
   )
 }
 
-function MediumRoundSquare() {
+function MediumRoundSquare({ style }: PropsStyle) {
   return (
-    <svg width="25" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <SvgIcon
+      sx={{ color: style.fill }}
+      width="25"
+      height="25"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <rect
+        stroke={style.stroke}
         x="0.75"
         y="0.75"
         width="22.5"
         height="22.5"
         rx="3.25"
-        stroke="#222222"
         strokeWidth="1.5"
       />
-    </svg>
+    </SvgIcon>
   )
 }
 
