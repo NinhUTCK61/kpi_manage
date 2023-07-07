7f20745bd40bdd35b4cabe9e93a4c9450cca9c8c
hoangtrung <trungnh@solashi.com>
2023-07-07 17:26:32 +0700

    fix: update node

--- src/libs/utils/misc.ts
+++ src/libs/utils/misc.ts
@@ -3,4 +3,4 @@ import { env } from '@/env.mjs'
 export const getImageUrl = (key: string) =>
   `${env.NEXT_PUBLIC_AWS_S3_ENDPOINT}/${key}?t=${Date.now()}`
 
-export const pxToNumber = (val: string) => Number(val.split('px')[0])
+export const pxToNumber = (val: string | undefined) => Number(val?.split('px')[0])
