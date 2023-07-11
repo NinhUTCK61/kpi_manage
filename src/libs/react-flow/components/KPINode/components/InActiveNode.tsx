b76ad4d8e3d1ac140b3efa80068ae44b00a2f79a
Nguyen Van Hieu <hieunv1@solashi.com>
2023-07-11 10:05:25 +0700

    feat: use useErrorState

--- src/libs/react-flow/components/KPINode/components/InActiveNode.tsx
+++ src/libs/react-flow/components/KPINode/components/InActiveNode.tsx
@@ -1,11 +1,11 @@
 import { ViewPortAction } from '@/features/node/constant'
-import { getSlugFromInputValue } from '@/libs/react-flow/helper/expression'
 import { useRFStore } from '@/libs/react-flow/hooks'
 import { formatNumber } from '@/libs/utils/format'
 import { Tooltip } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import { Position } from 'reactflow'
 import { useKPINodeContext } from '../context'
+import { useErrorState } from '../hooks'
 import { generateColors } from '../utils'
 import {
   LeftHandler,
@@ -19,24 +19,14 @@ import {
 const InActive: React.FC = () => {
   const viewportAction = useRFStore((state) => state.viewportAction)
   const d3Root = useRFStore((state) => state.d3Root)
-  const getKpiNodes = useRFStore((state) => state.getKpiNodes)
   const { data, isConnectable } = useKPINodeContext()
   const style = JSON.parse(data.node_style || '{}')
   const { t } = useTranslation('common')
   const d3Node = d3Root.find((node) => node.data.id === data.id)
   const hasChild = d3Node?.children && d3Node.children.length > 0
-
+  const { error, listSlugError } = useErrorState()
   const firstSlug = data.slug[0]
   const edgeColor = generateColors(firstSlug as string)
-
-  const slugs = (data.is_formula && getSlugFromInputValue(data.input_value as string)) || []
-  const listSlugError = []
-  if (slugs.length) {
-    listSlugError.push(
-      ...slugs.filter((slug) => !getKpiNodes().find((node) => node.data.slug === slug)),
-    )
-  }
-  const error = !!listSlugError.length
 
   return (
     <NodeInActiveContainer
