03fee206a9efddf865df4ec42661f684ae723104
hoangtrung <trungnh@solashi.com>
2023-07-11 17:51:41 +0700

    fix: node slug

--- src/libs/react-flow/helper/hierarchy.ts
+++ src/libs/react-flow/helper/hierarchy.ts
@@ -17,24 +17,57 @@ export function generateIds(d3Node: HierarchyFlowNode): void {
 
 export function generateNextIdByAdd(parentNode: HierarchyFlowNode): string {
   const { data, children } = parentNode
-  const { slug } = data.data
-  const existingSlugs = new Set(children?.map((child) => child.data.data.slug))
+  const parentSlug = data.data.slug
 
-  if (slug === 'root') {
-    let index = children?.length ?? 0
-    while (existingSlugs.has(String.fromCharCode(65 + index))) {
-      index++
+  if (parentSlug === 'root') {
+    if (!children || children.length === 0) {
+      return 'A'
+    } else {
+      const maxChildSlug = getMaxChildSlug(children)
+      const maxChildNumber = lettersToNumber(maxChildSlug)
+      return numberToLetters(maxChildNumber + 1)
     }
-    return String.fromCharCode(65 + index)
   } else {
-    let index = children?.length ?? 0 + 1
-    let nextSlug = `${slug}${index}`
-    while (existingSlugs.has(nextSlug)) {
-      index++
-      nextSlug = `${slug}${index}`
-    }
-    return nextSlug
+    const nextSlugNumber = (!children ? 0 : children.length) + 1
+    return parentSlug + nextSlugNumber
   }
+}
+
+function getMaxChildSlug(children: HierarchyFlowNode[]): string {
+  return children.reduce((max, child) => {
+    return lettersToNumber(child.data.data.slug) > lettersToNumber(max) ? child.data.data.slug : max
+  }, 'A')
+}
+
+function lettersToNumber(letters: string | undefined): number {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let number = 0
+
+  if (!letters) {
+    return number
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    const letterIndex = alphabet.indexOf(letters[i] as string)
+    if (letterIndex !== -1) {
+      number = number * 26 + (letterIndex + 1)
+    }
+  }
+
+  return number
+}
+
+function numberToLetters(number: number): string {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let letters = ''
+
+  while (number > 0) {
+    const remainder = (number - 1) % 26
+    letters = alphabet[remainder] + letters
+    number = Math.floor((number - 1) / 26)
+  }
+
+  return letters
 }
 
 export function _generateNextIdByFill(parentNode: HierarchyFlowNode): string {
@@ -92,7 +125,7 @@ function indexToSlug(index: number): string {
   return str
 }
 
-export const generateNextId = generateNextIdByFill
+export const generateNextId = generateNextIdByAdd
 
 export const generateNextNode = (parentNode: HierarchyFlowNode): KPINodeType => {
   return {
