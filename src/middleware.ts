7d1eec25a704a1ad6b039cf845c36405e0677aac
hoangtrung <trungnh@solashi.com>
2023-07-18 14:44:13 +0700

    feat: add basic auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,26 @@
-export { default } from 'next-auth/middleware'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { NextRequest, NextResponse } from 'next/server'
+
+// export
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1] as string
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (user === 'kpi_master' && pwd === 'kpi@master123') {
+      return withAuth(req as NextRequestWithAuth)
+    }
+  }
+  url.pathname = '/api/basic-auth'
+
+  return NextResponse.rewrite(url)
+}
+
+// export { withAuth } from 'next-auth/middleware'
 
 export const config = {
   matcher: ['/', '/file/:path*', '/profile', '/change-password', '/favorite'],
