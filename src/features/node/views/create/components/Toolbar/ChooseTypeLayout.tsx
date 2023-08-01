b99162adca0c6f082349734b4919f63bee33183b
thinh201 <thinh221201@gmail.com>
2023-08-01 16:53:30 +0700

    feat(KM-334): refactor code

--- src/features/node/views/create/components/Toolbar/ChooseTypeLayout.tsx
+++ src/features/node/views/create/components/Toolbar/ChooseTypeLayout.tsx
@@ -1,23 +1,35 @@
 import { base, customPrimary } from '@/libs/config/theme'
 import { useRFStore } from '@/libs/react-flow'
-import { Checkbox, FormControlLabel, FormGroup, Stack, Typography } from '@mui/material'
-import { ChangeEvent, useEffect, useLayoutEffect, useMemo, useState } from 'react'
+import {
+  FormControlLabel,
+  FormGroup,
+  Checkbox as MuiCheckbox,
+  Stack,
+  Typography,
+  styled,
+} from '@mui/material'
+import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
+import CheckedIcon from 'public/assets/svgs/checked_layout.svg'
+import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 import { useNodeUpdateHandler } from '../../../hooks'
 
 const ChooseTypeLayout: React.FC = () => {
+  const { t } = useTranslation('file')
   const nodeFocused = useRFStore((state) => state.nodeFocused)
+  const [checked, setChecked] = useState<boolean>(false)
+  const { updateReactFlowNode } = useNodeUpdateHandler()
   const nodeFocusedMemo = useMemo(() => {
     if (!nodeFocused || nodeFocused.type !== 'speech_ballon') return
     return nodeFocused
   }, [nodeFocused])
 
-  const [checked, setChecked] = useState<boolean>(true)
-
-  const { updateReactFlowNode } = useNodeUpdateHandler()
-
   const handleChange = (event: ChangeEvent) => {
-    const isCheckbox = (event.target as HTMLInputElement).checked 
+    const isCheckbox = (event.target as HTMLInputElement).checked
+
     if (!nodeFocusedMemo) return
+
+    setChecked(!checked)
     const nodeStyle = JSON.parse(nodeFocusedMemo.data.node_style || '{}')
     const isTextColorValid =
       nodeStyle.color !== base.white && nodeStyle.color !== customPrimary[700]
@@ -36,16 +48,10 @@ const ChooseTypeLayout: React.FC = () => {
     )
   }
 
-  const handleClick = ()=>{
-    if (!nodeFocusedMemo) return
-    setChecked(!checked)
-  }
-
-  useEffect(()=>{
-    if (!nodeFocusedMemo) return
-    setChecked(nodeFocusedMemo.data.layout === 'FILL' ? true: false)
-    console.log(nodeFocusedMemo.data.layout)
-  },[nodeFocusedMemo])
+  useEffect(() => {
+    if (!nodeFocusedMemo || nodeFocusedMemo.type !== 'speech_ballon') return
+    setChecked(nodeFocusedMemo.data.layout === 'FILL' ? true : false)
+  }, [nodeFocusedMemo])
 
   return (
     <Stack direction="row" alignItems="center" spacing={0.25} padding="0 8px">
@@ -54,8 +60,18 @@ const ChooseTypeLayout: React.FC = () => {
           sx={{
             mx: { xs: 'auto', sm: 0 },
           }}
-          control={<Checkbox checked={checked} sx={{ padding: 0 }} onChange={handleChange} onClick={handleClick}/>}
-          label={<Typography>Background Fill</Typography>}
+          control={
+            <Checkbox
+              checked={checked}
+              checkedIcon={<Image src={CheckedIcon} width={14} height={14} alt="checked" />}
+              onChange={handleChange}
+            />
+          }
+          label={
+            <Typography variant="body2" minWidth={120}>
+              {t('choose_layout_title')}
+            </Typography>
+          }
         />
       </FormGroup>
     </Stack>
@@ -63,3 +79,14 @@ const ChooseTypeLayout: React.FC = () => {
 }
 
 export { ChooseTypeLayout }
+
+const Checkbox = styled(MuiCheckbox)({
+  padding: 3,
+  width: 20,
+  height: 20,
+  borderRadius: 2,
+  color: 'transparent',
+  border: `1px solid ${customPrimary[700]}`,
+  background: 'white',
+  marginRight: 6,
+})
