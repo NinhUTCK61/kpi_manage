cbefc29cecf19bb7bfceb43c5743fb6838d1237d
thinh201 <thinh221201@gmail.com>
2023-08-07 13:46:56 +0700

    feat(KM-358): fix notification favorite

--- src/features/template/views/home/components/TemplateAction.tsx
+++ src/features/template/views/home/components/TemplateAction.tsx
@@ -5,7 +5,6 @@ import { formatDistance } from 'date-fns'
 import { enAU, ja } from 'date-fns/locale'
 import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
-import { enqueueSnackbar } from 'notistack'
 import LikeIcon from 'public/assets/svgs/like_star_1.svg'
 import UnLikeIcon from 'public/assets/svgs/unlike_star.svg'
 import { Ref } from 'react'
@@ -31,24 +30,11 @@ const TemplateAction: React.FC<TemplateActionTypes> = ({
   const {
     i18n: { language },
   } = useTranslation('home')
-  const { t } = useTranslation('home')
 
   const { mutation: mutationLike } = useLikeTemplate()
 
   const handleLike = () => {
-    mutationLike.mutate(
-      { id: template.template_id, is_favorite: !template.is_favorite },
-      {
-        onSuccess(data) {
-          if (data.is_favorite) {
-            enqueueSnackbar(t('favorite_template'), { variant: 'success' })
-            return
-          }
-
-          enqueueSnackbar(t('remove_favorite_template'), { variant: 'success' })
-        },
-      },
-    )
+    mutationLike.mutate({ id: template.template_id, is_favorite: !template.is_favorite })
   }
 
   return (
